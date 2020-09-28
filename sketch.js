93654904dcfc38be37c58192748d760973be5a7d
chiara <chiara7.brambilla@mail.polimi.it>
2020-09-28 14:41:59 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,58 @@
-function preload(){
-  // put preload code here
-}
+function preload(){}
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  cnv = createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+  background(10, 34, 56); //colore sfondo
+  frameRate(60);
 }
 
+let a = false;
+let s = 'try to move your mouse...';
+let t = 'click to stop and press the down arrow to download it ...';
+
 function draw() {
-  // put drawing code here
+colorMode(HSB,100);
+
+//cambia colore in base alla posizione del mouse
+stroke(100*mouseX/windowWidth, 255, 100, 1000/frameCount); //stroke(v1,v2,v3,[alpha])
+
+//download
+    if (keyIsDown(DOWN_ARROW)) {
+        saveCanvas(cnv,'Rose.jpg'); // saveCanvas(selectedCanvas,[filename],[extension])
+      }
+
+//TESTO
+push();
+   noStroke();
+   fill(120);
+   textSize(12);
+   textAlign(CENTER); //textAlign(horizAlign,[vertAlign])
+   if(frameCount <= 360 )
+    { text(s, width/2, height/16);
+   } else {
+      text(t, width/2, height/11);}
+pop();
+
+ //rosa
+noFill();
+translate(width/2, height/2);//posizione nello spazio centrale
+rotate(frameCount);
+translate(50, 0);
+scale(frameCount/500);
+ellipse(0 ,0, 50);
+translate(50, 0);
+ellipse(0 ,10, 100);
 }
+
+//start and stop
+function mousePressed() {
+  if(a === false) {
+    //  noLoop();
+        frameRate(0);
+        a = true;
+  } else {
+     frameRate(60);
+       a = false;
+     }
+ }
